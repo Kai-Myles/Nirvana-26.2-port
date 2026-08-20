78761e2c675cd179217b55119e05d44422663982
Kai Myles <myleskai011@gmail.com>
2026-08-20 10:44:26 -0300

    Exclude obsolete Equipable marker from common compilation

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -15,6 +15,7 @@ sourceSets {
         java {
             exclude("galena/nirvana/config/ForgeClientConfig.java")
             exclude("galena/nirvana/config/ForgeCommonConfig.java")
+            exclude("galena/nirvana/world/item/ArmorLike.java")
         }
     }
 }
