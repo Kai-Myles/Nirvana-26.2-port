203925a70f8267860b5223a49a7debb12c6e5805
Kai Myles <myleskai011@gmail.com>
2026-08-20 10:43:23 -0300

    Port deerstalker to 26.2 equipment components

--- common/src/main/java/galena/nirvana/world/item/DeerStalkerItem.java
+++ common/src/main/java/galena/nirvana/world/item/DeerStalkerItem.java
@@ -1,68 +1,31 @@
 package galena.nirvana.world.item;
 
-import com.google.common.base.Suppliers;
-import galena.nirvana.NirvanaConstants;
 import galena.nirvana.index.NirvanaItems;
-import java.util.function.Supplier;
-import net.minecraft.core.Holder;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.EquipmentSlotGroup;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.DispenserBlock;
 
-public class DeerStalkerItem extends Item implements ArmorLike {
-
-    private final Supplier<ItemAttributeModifiers> modifiers;
+public class DeerStalkerItem extends Item {
 
     public DeerStalkerItem(Properties properties) {
-        super(properties);
-        DispenserBlock.registerBehavior(this, ArmorItem.DISPENSE_ITEM_BEHAVIOR);
-
-        this.modifiers = Suppliers.memoize(() -> {
-            var builder = ItemAttributeModifiers.builder();
-            builder.add(Attributes.ARMOR, new AttributeModifier(NirvanaConstants.createId("armor"), ArmorMaterials.LEATHER.value().getDefense(ArmorItem.Type.HELMET), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD);
-            builder.add(Attributes.ARMOR_TOUGHNESS, new AttributeModifier(NirvanaConstants.createId("toughness"), ArmorMaterials.LEATHER.value().toughness(), AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.HEAD);
-            return builder.build();
-        });
+        super(properties
+                .durability(65)
+                .enchantable(15)
+                .equippable(EquipmentSlot.HEAD)
+                .repairable(NirvanaItems.HEMP_CLOTH));
     }
 
-    public boolean isValidRepairItem(ItemStack stack, ItemStack with) {
-        return NirvanaItems.HEMP_CLOTH.isIn(with);
-    }
-
+    @Override
     public InteractionResult use(Level level, Player player, InteractionHand hand) {
-        return swapWithEquipmentSlot(this, level, player, hand);
+        var stack = player.getItemInHand(hand);
+        var equippable = stack.get(net.minecraft.core.component.DataComponents.EQUIPPABLE);
+        if (equippable != null) {
+            return equippable.swapWithEquipmentSlot(stack, player);
+        }
+        return InteractionResult.PASS;
     }
-
-    @Override
-    public int getEnchantmentValue() {
-        return ArmorMaterials.LEATHER.value().enchantmentValue();
-    }
-
-    @Override
-    public Holder<SoundEvent> getEquipSound() {
-        return ArmorMaterials.LEATHER.value().equipSound();
-    }
-
-    @Override
-    public EquipmentSlot getEquipmentSlot() {
-        return EquipmentSlot.HEAD;
-    }
-
-    @Override
-    public ItemAttributeModifiers getDefaultAttributeModifiers() {
-        return modifiers.get();
-    }
-
 }
